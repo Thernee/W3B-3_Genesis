6ad6087ee6c40adaf62f326561bc4d4746cf9ae8
Abubakar Adam Sani <Athernee@gmail.com>
2025-02-04 15:27:46 +0100

    Implemented custom library for ETH conversion

--- contracts/Funding/Funding.sol
+++ contracts/Funding/Funding.sol
@@ -7,11 +7,14 @@ pragma solidity ^0.8.18;
    * Specify minimum USD funding value
 **/
 
-// Import AggregatorV3Interface interface
-import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import { EthConverter } from "./EthConverter.sol";
 
 contract Funding {
+    // 
+    using EthConverter for uint256;
+
     uint256 minimum = 5e18;
+
     address[] public fundersList;
     
     // Link address to amount funded
@@ -19,39 +22,12 @@ contract Funding {
 
     // Func. to receive and record funding
     function fund() public payable {
-        require(getPriceUsd(msg.value) >= minimum, "Not enough ETH sent");
+        require(msg.value.getPriceUsd() >= minimum, "Not enough ETH sent");
 
         // Store each funders's address
         fundersList.push(msg.sender);
-
+ 
         // Map all donations to addresses
         fundingRecords[msg.sender] = fundingRecords[msg.sender] + msg.value;
-    }
-
-    // Get current, real-world ETH price in terms of wei
-    function getETHPrice() public view returns(uint256) {
-        // ETH/Sepolia address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface datafeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-
-        // Destructure and get only price from the returned data
-        // Price is returned with 8 decimals
-        (,int256 price,,,) = datafeed.latestRoundData();
-
-        // Add 10 decimals to standardize (ETH in wei) the price to 18 decimal places (8 + 10)
-        return uint256(price * 1e10);
-    }
-
-    // Get ETH price in USD
-    function getPriceUsd(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getETHPrice();
-
-        // Covert given ETH amount to wei
-        ethAmount = ethAmount * 1e18;
-        uint256 priceUsd = (ethAmount * ethPrice) / 1e18;
-        return priceUsd;
-    }
-    
-    function getVersion() public view returns(uint256) {
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 }
